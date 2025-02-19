50dea46849fae3908497e208d0754653cf55fca4
FreseEnzo <enzo.frese@hardwario.com>
2025-02-19 19:54:12 -0300

    web: Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'FreseTech',
-  tagline: 'Your ',
+  tagline: 'Macaco',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
